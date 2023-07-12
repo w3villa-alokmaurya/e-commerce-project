864d5f157439f2b22141b05087446315e95a450a
Alok Maurya <alok.maurya@w3villa.com>
2023-07-12 17:02:27 +0530

    landing page desktop view completed

--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,82 @@
-var owl = $('.owl-carousel');
+var owl = $('#brands');
 owl.owlCarousel({
-    items:9,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:1000,
-    autoplayHoverPause:true
+    items: 9,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    autoplayHoverPause: true
 });
-$('.play').on('click',function(){
-    owl.trigger('play.owl.autoplay',[1000])
+$('.play').on('click', function () {
+    owl.trigger('play.owl.autoplay', [1000])
 })
-$('.stop').on('click',function(){
+$('.stop').on('click', function () {
+    owl.trigger('stop.owl.autoplay')
+})
+
+var owl = $('#blog-cards');
+owl.owlCarousel({
+    items: 3,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true
+});
+$('.play').on('click', function () {
+    owl.trigger('play.owl.autoplay', [1000])
+})
+$('.stop').on('click', function () {
+    owl.trigger('stop.owl.autoplay')
+})
+
+var owl = $('#banner-slider');
+owl.owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    dots:true,
+});
+$('.play').on('click', function () {
+    owl.trigger('play.owl.autoplay', [1000])
+})
+$('.stop').on('click', function () {
+    owl.trigger('stop.owl.autoplay')
+})
+
+
+var owl = $('#testimonials');
+owl.owlCarousel({
+    items: 3,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true
+});
+$('.play').on('click', function () {
+    owl.trigger('play.owl.autoplay', [1000])
+})
+$('.stop').on('click', function () {
+    owl.trigger('stop.owl.autoplay')
+})
+
+var owl = $('#most-viewed');
+owl.owlCarousel({
+    items: 4,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    dots:false,
+});
+$('.play').on('click', function () {
+    owl.trigger('play.owl.autoplay', [1000])
+})
+$('.stop').on('click', function () {
     owl.trigger('stop.owl.autoplay')
 })
