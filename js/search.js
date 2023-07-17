95d1c96a2265bb0178c610f21560707abe4f68ff
Alok Maurya <alok.maurya@w3villa.com>
2023-07-17 11:54:42 +0530

    search page completed

---
+++ js/search.js
@@ -0,0 +1,10 @@
+function listview(){
+    document.getElementById('listview').classList.add('displaynone');
+    document.getElementById('gridview').classList.add('displayblock');
+}
+
+
+function gridview(){
+    document.getElementById('gridview').classList.add('displaynone');
+    document.getElementById('listview').classList.add('displayblock');
+}
