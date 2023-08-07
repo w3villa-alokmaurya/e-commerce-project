3e74e340e2207e8ea3f86ed3414b934f7e0f2c48
Alok Maurya <alok.maurya@w3villa.com>
2023-08-07 15:16:24 +0530

    Newletter form is working and done with js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
+
+// scroll to top 
 const scrollToTop = () => {
     window.scroll({top: 0, left: 0, behavior: 'smooth'});
     }
+
+
+// newsletter form
+const form = document.getElementById('newsletter-form');
+const newsletter = () =>{
+   
+    const email = document.getElementById('newsletter-email').value;
+    const storedNewsletterEmail= localStorage.getItem('newsletter_email');
+    if(email==''){
+        alert("Please fill all the details");
+    }else if(storedNewsletterEmail==email){
+        alert("Already Subscribed");
+    }else {
+        localStorage.setItem('newsletter_email', email);
+        alert("Subscribed Successfully");
+    }
+    
+
+}
+form.addEventListener("submit",newsletter);
+
   
