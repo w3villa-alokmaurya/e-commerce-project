466dcc5d408fac047e03a9afc6caf309530bbed2
Alok Maurya <alok.maurya@w3villa.com>
2023-08-10 18:46:41 +0530

    search added in console

--- js/main.js
+++ js/main.js
@@ -486,6 +486,31 @@ testimonialCards();
 //Search form
 
 
+const search = (key) => {
+
+  fetchData(apiUrl)
+  .then(data => {
+    let fullarr = data['featured'].concat(data['latest']).concat(data['bestsellers']).concat(data['specials']);
+    // console.log(fullarr);
+    fullarr.forEach(value=>{
+       // Find all the product with the search term
+    
+      if (value.title.toLowerCase().includes(key.toLowerCase())) {
+        var result = value;
+        console.log(result);
+      }
+
+    })
+  })
+}
+const searchForm = document.getElementById('search-form');
+
+const searchHandle = () => {
+  let searchInput = document.getElementById('search-input').value;
+  search(searchInput);
+
+}
+// searchForm.addEventListener('click', searchHandle);
 
 
 //products data
